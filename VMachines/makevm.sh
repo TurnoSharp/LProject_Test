7149e030877ccd22b8763ca772e8a89f3a1abe8f
TurnoSharp <turnosharp@gmail.com>
2022-05-13 18:58:45 +0300

    add makevm.sh in ./VMachines for automatical make VM Vagrant

---
+++ VMachines/makevm.sh
@@ -0,0 +1,19 @@
+
+#!bin/#!/usr/bin/env bash
+
+echo "_________________________________"
+echo "Tell the serial number of the VM:"
+echo "_________________________________"
+read n
+
+#make directory for new VM and vagrant init
+mkdir ./vagrant_vm$n
+cd ./vagrant_vm$n
+vagrant init
+
+#add to .gitignore extra files
+echo "/VMachines/vagrant_vm$n/.vagrant" >> /home/turnosharp/LProject_Test/.gitignore
+
+#configure Vagrantfile
+sed -i "12r ../config.vm.provision" ./Vagrantfile
+sed -i "41c\config.vm.network "private_network", ip: "192.168.56.2$n""  ./Vagrantfile
